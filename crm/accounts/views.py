8202f449125535dedcff99548e4128f88b0a6391
Tejas Nikhar <tejastn10@gmail.com>
2021-04-21 13:02:38 +0530

    📦 UPDATE: View with data

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
 # Create your views here.
 
+from .models import *
+
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    orders_delivered = orders.filter(status='Delivered').count()
+    orders_pending = orders.filter(status='Pending').count()
+
+    context = {
+        'customers': customers,
+        'orders': orders,
+        'total_customers': total_customers,
+        'total_orders': total_orders,
+        'orders_delivered': orders_delivered,
+        'orders_pending': orders_pending,
+    }
+
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+
+    return render(request, 'accounts/products.html', {'products': products})
 
 
 def customer(request):
