37374c535a04992feb47f1943fa7f13a20ff0e58
Tejas Nikhar <tejastn10@gmail.com>
2021-04-23 14:44:12 +0530

    📦 UPDATE: Views and URLs

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('products/', views.products, name="products"),
     path('customer/<str:pk>', views.customer, name="customer"),
+    path('new_order/', views.createOrder, name="new_order"),
+    path('update_order/<str:pk>', views.updateOrder, name="update_order"),
+    path('delete_order/<str:pk>', views.deleteOrder, name="delete_order"),
 ]
--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.db import connection
+from django.shortcuts import render, redirect
 # Create your views here.
 
 from .models import *
+from .forms import OrderForm
 
 
 def home(request):
@@ -43,3 +45,52 @@ def customer(request, pk):
     }
 
     return render(request, 'accounts/customer.html', context)
+
+
+def createOrder(request):
+    form = OrderForm()
+
+    if request.method == 'POST':
+        # print('Printing Form Data: ', request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/order_form.html', context)
+
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        # print('Printing Form Data: ', request.POST)
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'accounts/order_form.html', context)
+
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+
+    if request.method == 'POST':
+        order.delete()
+        return redirect("/")
+
+    context = {
+        'item': order,
+    }
+
+    return render(request, 'accounts/delete.html', context)
