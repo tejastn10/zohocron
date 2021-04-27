30644f3b32753b69f9fe900bc450266f93413d1d
Tejas Nikhar <tejastn10@gmail.com>
2021-04-27 21:31:34 +0530

    🧱 UPDATE: Order Model & migrations

--- crm/accounts/models.py
+++ crm/accounts/models.py
@@ -51,6 +51,7 @@ class Order(models.Model):
     )
     status = models.CharField(max_length=200, null=True, choices=STATUS)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    note = models.CharField(max_length=1000, null=True)
 
     def __str__(self):
         return self.product.name
