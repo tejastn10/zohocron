9ad78b28fc6cd6bab46c41e25c318ca06a9f0cfb
Tejas Nikhar <tejastn10@gmail.com>
2021-05-01 11:32:02 +0530

    📦 NEW: User Login functionality

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
     path('products/', views.products, name="products"),
     path('customer/<str:pk>', views.customer, name="customer"),
--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 from .models import *
 from .filters import OrderFilter
@@ -33,7 +34,25 @@ def loginPage(request):
 
     }
 
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid credentials!')
+            return render(request, 'accounts/login.html', context)
+
     return render(request, 'accounts/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 
 def home(request):
